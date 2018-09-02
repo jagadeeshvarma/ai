2b01845e1cd3a644cc724f1bdfccca2b2ae965f6
Jagadeesh Varma <jagadeesh.ifs@gmail.com>
2018-09-02 20:49:17 +0530

    few more assignments on lists

---
+++ py/sort_list_of_tuples.py
@@ -0,0 +1,5 @@
+# Write a program to Sort a List of Tuples in Increasing Order by the Last Element in Each Tuple
+
+tuple_list = [eval(x) for x in input('enter list of tuples seperated by space: ').split()]
+
+print(tuple_list)
